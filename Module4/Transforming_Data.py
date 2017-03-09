1866c01c6d7d180a94be73bd572ed0fdcad5c6b9
fuat <fuatarslan75@gmail.com>
2017-03-09 18:32:34 +0300

    added PCA for kidney disease

--- Module4/Transforming_Data.py
+++ Module4/Transforming_Data.py
@@ -133,83 +133,7 @@ plt.show()
 
 
 
-# In[15]:
-
-import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib
-import assignment2_helper as helper
-
-
-
-
-# TODO: Load up the dataset and remove any and all
-# Rows that have a nan. You should be a pro at this
-# by now ;-)
-#
-# QUESTION: Should the id column be included as a
-# feature?
-#
-# .. your code here ..
-
-kidney_df = pd.read_csv('Datasets\\kidney_disease.csv', index_col=0).dropna(axis=0)
-kidney_df.head()
-
-
-# In[16]:
-
-len(kidney_df.columns)
-
-
-# In[17]:
-
-len(kidney_df)
-
-
 # In[34]:
-
-# Create some color coded labels; the actual label feature
-# will be removed prior to executing PCA, since it's unsupervised.
-# You're only labeling by color so you can see the effects of PCA
-labels = ['red' if i=='ckd' else 'green' for i in kidney_df.classification]
-
-
-# TODO: Use an indexer to select only the following columns:
-#       ['bgr','wc','rc']
-#
-# .. your code here ..
-
-kidney_df1 = kidney_df[['bgr','wc','rc']]
-kidney_df1.head()
-
-
-# In[35]:
-
-# TODO: Print out and check your dataframe's dtypes. You'll might
-# want to set a breakpoint after you print it out so you can stop the
-# program's execution.
-#
-# You can either take a look at the dataset webpage in the attribute info
-# section: https://archive.ics.uci.edu/ml/datasets/Chronic_Kidney_Disease
-# or you can actually peek through the dataframe by printing a few rows.
-# What kind of data type should these three columns be? If Pandas didn't
-# properly detect and convert them to that data type for you, then use
-# an appropriate command to coerce these features into the right type.
-#
-# .. your code here ..
-##checked them, bgr is float(true), wc false(should be float), rc false(should be numeric)
-
-kidney_df1.dtypes
-kidney_df1.head()
-
-
-# In[36]:
-
-kidney_df1[['wc', 'rc']] = kidney_df1[['wc', 'rc']].apply(pd.to_numeric)
-kidney_df1.dtypes
-
-
-# In[ ]:
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -234,15 +158,14 @@ scaleFeatures = False
 #
 # .. your code here ..
 
-kidney_df = pd.read_csv('Datasets\\kidney_disease.csv')
-kidney_df.head()
+kidney_df = pd.read_csv('Datasets\\kidney_disease.csv', index_col=0).dropna(axis=0)
 
 
 
 # Create some color coded labels; the actual label feature
 # will be removed prior to executing PCA, since it's unsupervised.
 # You're only labeling by color so you can see the effects of PCA
-labels = ['red' if i=='ckd' else 'green' for i in df.classification]
+labels = ['red' if i=='ckd' else 'green' for i in kidney_df.classification]
 
 
 # TODO: Use an indexer to select only the following columns:
@@ -251,6 +174,7 @@ labels = ['red' if i=='ckd' else 'green' for i in df.classification]
 # .. your code here ..
 
 
+kidney_df1 = kidney_df[['bgr','wc','rc']]
 
 # TODO: Print out and check your dataframe's dtypes. You'll might
 # want to set a breakpoint after you print it out so you can stop the
@@ -264,8 +188,10 @@ labels = ['red' if i=='ckd' else 'green' for i in df.classification]
 # an appropriate command to coerce these features into the right type.
 #
 # .. your code here ..
-
-
+kidney_df1.dtypes
+#kidney_df1.head()
+kidney_df1[['wc', 'rc']] = kidney_df1[['wc', 'rc']].apply(pd.to_numeric)
+#kidney_df1.dtypes
 
 # TODO: PCA Operates based on variance. The variable with the greatest
 # variance will dominate. Go ahead and peek into your data using a
@@ -277,7 +203,7 @@ labels = ['red' if i=='ckd' else 'green' for i in df.classification]
 # you probably didn't complete the previous step properly.
 #
 # .. your code here ..
-
+kidney_df1.describe()
 
 
 # TODO: This method assumes your dataframe is called df. If it isn't,
@@ -285,8 +211,8 @@ labels = ['red' if i=='ckd' else 'green' for i in df.classification]
 # just yet though!
 #
 # .. your code adjustment here ..
-if scaleFeatures: df = helper.scaleFeatures(df)
-
+#if helper.scaleFeatures: df = helper.scaleFeatures(df)
+if helper.scaleFeatures: kidney_df1 = helper.scaleFeatures(kidney_df1)
 
 
 # TODO: Run PCA on your dataset and reduce it to 2 components
@@ -294,7 +220,12 @@ if scaleFeatures: df = helper.scaleFeatures(df)
 # and that the results of your transformation are saved in 'T'.
 #
 # .. your code here ..
-
+from sklearn.decomposition import PCA
+pca = PCA(n_components=2, svd_solver='full')
+pca.fit(kidney_df1)
+T = pca.transform(kidney_df1)
+#print T.shape
+#print kidney_df1.shape
 
 # Plot the transformed data as a scatter plot. Recall that transforming
 # the data will result in a NumPy NDArray. You can either use MatPlotLib
@@ -306,11 +237,129 @@ if scaleFeatures: df = helper.scaleFeatures(df)
 #
 # Since we transformed via PCA, we no longer have column names. We know we
 # are in P.C. space, so we'll just define the coordinates accordingly:
-ax = helper.drawVectors(T, pca.components_, df.columns.values, plt, scaleFeatures)
+ax = helper.drawVectors(T, pca.components_, kidney_df1.columns.values, plt, scaleFeatures)
 T = pd.DataFrame(T)
 T.columns = ['component1', 'component2']
 T.plot.scatter(x='component1', y='component2', marker='o', c=labels, alpha=0.75, ax=ax)
 plt.show()
+
+
+
+
+# In[35]:
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+import assignment2_helper as helper
+
+# Look pretty...
+# matplotlib.style.use('ggplot')
+plt.style.use('ggplot')
+
+
+# Do * NOT * alter this line, until instructed!
+scaleFeatures = True
+
+
+# TODO: Load up the dataset and remove any and all
+# Rows that have a nan. You should be a pro at this
+# by now ;-)
+#
+# QUESTION: Should the id column be included as a
+# feature?
+#
+# .. your code here ..
+
+kidney_df = pd.read_csv('Datasets\\kidney_disease.csv', index_col=0).dropna(axis=0)
+
+
+
+# Create some color coded labels; the actual label feature
+# will be removed prior to executing PCA, since it's unsupervised.
+# You're only labeling by color so you can see the effects of PCA
+labels = ['red' if i=='ckd' else 'green' for i in kidney_df.classification]
+
+
+# TODO: Use an indexer to select only the following columns:
+#       ['bgr','wc','rc']
+#
+# .. your code here ..
+
+
+kidney_df1 = kidney_df[['bgr','wc','rc']]
+
+# TODO: Print out and check your dataframe's dtypes. You'll might
+# want to set a breakpoint after you print it out so you can stop the
+# program's execution.
+#
+# You can either take a look at the dataset webpage in the attribute info
+# section: https://archive.ics.uci.edu/ml/datasets/Chronic_Kidney_Disease
+# or you can actually peek through the dataframe by printing a few rows.
+# What kind of data type should these three columns be? If Pandas didn't
+# properly detect and convert them to that data type for you, then use
+# an appropriate command to coerce these features into the right type.
+#
+# .. your code here ..
+kidney_df1.dtypes
+#kidney_df1.head()
+kidney_df1[['wc', 'rc']] = kidney_df1[['wc', 'rc']].apply(pd.to_numeric)
+#kidney_df1.dtypes
+
+# TODO: PCA Operates based on variance. The variable with the greatest
+# variance will dominate. Go ahead and peek into your data using a
+# command that will check the variance of every feature in your dataset.
+# Print out the results. Also print out the results of running .describe
+# on your dataset.
+#
+# Hint: If you don't see all three variables: 'bgr','wc' and 'rc', then
+# you probably didn't complete the previous step properly.
+#
+# .. your code here ..
+kidney_df1.describe()
+
+
+# TODO: This method assumes your dataframe is called df. If it isn't,
+# make the appropriate changes. Don't alter the code in scaleFeatures()
+# just yet though!
+#
+# .. your code adjustment here ..
+#if helper.scaleFeatures: df = helper.scaleFeatures(df)
+if helper.scaleFeatures: kidney_df1 = helper.scaleFeatures(kidney_df1)
+
+
+# TODO: Run PCA on your dataset and reduce it to 2 components
+# Ensure your PCA instance is saved in a variable called 'pca',
+# and that the results of your transformation are saved in 'T'.
+#
+# .. your code here ..
+from sklearn.decomposition import PCA
+pca = PCA(n_components=2, svd_solver='full')
+pca.fit(kidney_df1)
+T = pca.transform(kidney_df1)
+#print T.shape
+#print kidney_df1.shape
+
+# Plot the transformed data as a scatter plot. Recall that transforming
+# the data will result in a NumPy NDArray. You can either use MatPlotLib
+# to graph it directly, or you can convert it to DataFrame and have pandas
+# do it for you.
+#
+# Since we've already demonstrated how to plot directly with MatPlotLib in
+# Module4/assignment1.py, this time we'll convert to a Pandas Dataframe.
+#
+# Since we transformed via PCA, we no longer have column names. We know we
+# are in P.C. space, so we'll just define the coordinates accordingly:
+ax = helper.drawVectors(T, pca.components_, kidney_df1.columns.values, plt, scaleFeatures)
+T = pd.DataFrame(T)
+T.columns = ['component1', 'component2']
+T.plot.scatter(x='component1', y='component2', marker='o', c=labels, alpha=0.75, ax=ax)
+plt.show()
+
+
+
+
+# In[ ]:
 
 
 
